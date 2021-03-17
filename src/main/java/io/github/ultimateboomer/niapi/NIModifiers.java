f97de6b4e26f45650864898173428f673bc2c822
Steven Cao <caohc2017@gmail.com>
2021-03-17 12:12:03 -0400

    Cleanup, change fabric.mod.json

--- src/main/java/io/github/ultimateboomer/niapi/NIModifiers.java
+++
@@ -1,5 +0,0 @@
-package io.github.ultimateboomer.niapi;
-
-public final class NIModifiers {
-
-}
--- src/main/java/io/github/ultimateboomer/niapi/NativeImageUtil.java
+++ src/main/java/io/github/ultimateboomer/niapi/NativeImageUtil.java
@@ -40,9 +40,6 @@ public final class NativeImageUtil {
         // Prepare new image
         NativeImage newImage = new NativeImage(image.getFormat(), (int) (image.getWidth() * widthMul),
                 (int) (image.getHeight() * heightMul), image.isStbImage);
-//        int newWidth = (int) ();
-//        int newHeight = (int) ();
-//        long newSize = (long) newWidth * newHeight * image.getFormat().getChannelCount();
 
         // Upscale with STBImageResize
         STBImageResize.nstbir_resize_uint8(image.pointer, image.getWidth(), image.getHeight(), 0,
@@ -83,17 +80,12 @@ public final class NativeImageUtil {
                 (int) (image.height * heightMul), image.isStbImage);
 
         // Update image attributes
-//        image.width *= widthMul;
-//        image.height *= heightMul;
-//        image.sizeBytes = (long) image.getWidth() * image.getHeight() * image.getFormat().getChannelCount();
-//        image.pointer = MemoryUtil.nmemAlloc(image.sizeBytes);
-
         if (image.getWidth() * image.getHeight() > newImage.getWidth() * newImage.getHeight()) {
             // Scaled image is larger than original image
-            for (int x = 0; x < image.getWidth(); ++x) {
-                for (int y = 0; y < image.getHeight(); ++y) {
-                    for (int x1 = (int) (x * widthMul); x1 < (x + 1) * widthMul; ++x1) {
-                        for (int y1 = (int) (y * heightMul); y1 < (y + 1) * heightMul; ++y1) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    for (int x1 = (int) (x * widthMul); x1 < (x + 1) * widthMul; x1++) {
+                        for (int y1 = (int) (y * heightMul); y1 < (y + 1) * heightMul; y1++) {
                             newImage.setPixelColor(x1, y1, image.getPixelColor(x, y));
                         }
                     }
@@ -101,8 +93,8 @@ public final class NativeImageUtil {
             }
         } else {
             // Scaled image is smaller than original image
-            for (int x = 0; x < newImage.getWidth(); ++x) {
-                for (int y = 0; y < newImage.getHeight(); ++y) {
+            for (int x = 0; x < newImage.getWidth(); x++) {
+                for (int y = 0; y < newImage.getHeight(); y++) {
                     newImage.setPixelColor(x, y, image.getPixelColor((int) (x / widthMul), (int) (y / heightMul)));
                 }
             }
@@ -142,8 +134,8 @@ public final class NativeImageUtil {
         }
 
         // Make copy of original image
-        NativeImage newImage = new NativeImage(image.getFormat(), (int) (image.width * widthMul),
-                (int) (image.height * heightMul), image.isStbImage);
+        NativeImage newImage = new NativeImage(image.getFormat(), (int) (image.getWidth() * widthMul),
+                (int) (image.getHeight() * heightMul), image.isStbImage);
 
         int channels = image.getFormat().getChannelCount();
         int alphaChannel = image.getFormat().getAlphaChannelOffset() / 8;
@@ -151,32 +143,72 @@ public final class NativeImageUtil {
 
         if (newImage.getWidth() * newImage.getHeight() > image.getWidth() * image.getHeight()) {
             // Scaled image is larger than original image
-            // Temporary solution with STBImageResize
-            STBImageResize.nstbir_resize_uint8(image.pointer, image.getWidth(), image.getHeight(), 0,
-                    newImage.pointer, newImage.getWidth(), newImage.getHeight(), 0,
-                    image.getFormat().getChannelCount());
-//            int[][] array = toArray(image);
-//
-//            for (int x = 0; x < image.getWidth(); ++x) {
-//                for (int y = 0; y < image.getHeight(); ++y) {
-//                    for (int x1 = (int) (x * widthMul); x1 < (x + 1) * widthMul; ++x1) {
-//                        for (int y1 = (int) (y * heightMul); y1 < (y + 1) * heightMul; ++y1) {
-//                            newImage.setPixelColor(x1, y1, image.getPixelColor(x, y));
-//                        }
-//                    }
-//                }
-//            }
+            ByteBuffer buffer = ByteBuffer.allocate(channels);
+            buffer.order(ByteOrder.LITTLE_ENDIAN);
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    ByteBuffer[][] adjacentColors = new ByteBuffer[2][2];
+
+                    for (int x1 = 0; x1 <= 1; x1++) {
+                        for (int y1 = 0; y1 <= 1; y1++) {
+                            int color = image.getPixelColor((x + x1) % image.getWidth(),
+                                    (y + y1) % image.getHeight());
+                            adjacentColors[x1][y1] = ByteBuffer.allocate(channels);
+                            adjacentColors[x1][y1].order(ByteOrder.LITTLE_ENDIAN);
+                            adjacentColors[x1][y1].putInt(color);
+                        }
+                    }
+
+                    for (int x1 = (int) (x * widthMul); x1 < (x + 1) * widthMul; x1++) {
+                        for (int y1 = (int) (y * heightMul); y1 < (y + 1) * heightMul; y1++) {
+                            double localX = (x1 - x * widthMul) / widthMul;
+                            double localY = (y1 - y * heightMul) / heightMul;
+
+                            int[] totalColor = new int[channels];
+                            if (hasAlphaChannel)
+                                totalColor[alphaChannel] = 0xFF;
+
+                            for (int x2 = 0; x2 <= 1; x2++) {
+                                for (int y2 = 0; y2 <= 1; y2++) {
+                                    for (int i = 0; i < channels; i++) {
+                                        ByteBuffer color = adjacentColors[x2][y2];
+                                        int f = Byte.toUnsignedInt(color.get(i));
+                                        if (i != alphaChannel) {
+
+                                            f *= (x2 == 1 ? localX : (1 - localX));
+                                            f *= (y2 == 1 ? localY : (1 - localY));
+                                            totalColor[i] += f;
+                                        } else {
+                                            totalColor[i] = adjacentColors[0][0].get(i);
+                                        }
+
+
+                                    }
+                                }
+                            }
+                            for (int i = 0; i < channels; i++) {
+                                buffer.put((byte) totalColor[i]);
+                            }
+
+                            buffer.flip();
+                            newImage.setPixelColor(x1, y1, buffer.getInt());
+                            buffer.clear();
+                        }
+                    }
+                }
+            }
+
         } else {
             // Scaled image is smaller than original image
             ByteBuffer buffer = ByteBuffer.allocate(channels);
             buffer.order(ByteOrder.LITTLE_ENDIAN);
-            for (int x = 0; x < newImage.getWidth(); ++x) {
-                for (int y = 0; y < newImage.getHeight(); ++y) {
+            for (int x = 0; x < newImage.getWidth(); x++) {
+                for (int y = 0; y < newImage.getHeight(); y++) {
                     int i = 0;
                     int[] totalColor = new int[channels];
 
-                    for (int x1 = (int) (x / widthMul); x1 < (x + 1) / widthMul; ++x1) {
-                        for (int y1 = (int) (y / heightMul); y1 < (y + 1) / heightMul; ++y1) {
+                    for (int x1 = (int) (x / widthMul); x1 < (x + 1) / widthMul; x1++) {
+                        for (int y1 = (int) (y / heightMul); y1 < (y + 1) / heightMul; y1++) {
                             buffer.putInt(image.getPixelColor(x1, y1));
 
                             if (hasAlphaChannel && buffer.get(alphaChannel) == 0) {
@@ -184,7 +216,7 @@ public final class NativeImageUtil {
                                 continue;
                             }
 
-                            for (int j = 0; j < channels; ++j) {
+                            for (int j = 0; j < channels; j++) {
                                 if (j == alphaChannel) {
                                     totalColor[j] = Math.max(Byte.toUnsignedInt(buffer.get(j)), totalColor[j]);
                                 } else {
@@ -199,13 +231,11 @@ public final class NativeImageUtil {
                     }
 
                     if (i > 0) {
-                        for (int j = 0; j < 4; ++j) {
+                        for (int j = 0; j < channels; j++) {
                             if (j == alphaChannel) {
                                 buffer.put((byte) totalColor[j]);
-                            } else if (j < totalColor.length) {
-                                buffer.put((byte) (Math.sqrt((double) totalColor[j] / i)));
                             } else {
-                                buffer.put((byte) 0);
+                                buffer.put((byte) (Math.sqrt((double) totalColor[j] / i)));
                             }
                         }
                         buffer.flip();
@@ -226,8 +256,8 @@ public final class NativeImageUtil {
      * @param keepTransparency whether to use original transparency for masking
      */
     public static void replaceImage(NativeImage image, NativeImage replacer, boolean keepTransparency) {
-        for (int x = 0; x < image.getWidth(); ++x) {
-            for (int y = 0; y < image.getHeight(); ++y) {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
                 int color = replacer.getPixelColor(x % replacer.getWidth(), y % replacer.getHeight());
                 if (keepTransparency) {
                     color &= 0x00FFFFFF;
@@ -266,8 +296,8 @@ public final class NativeImageUtil {
 
         int[][] array = new int[width][height];
         ByteBuffer buffer = ByteBuffer.allocate(channels);
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 int offset = (x + y * image.width) * channels;
 
                 for (int i = 0; i < channels; ++i) {
@@ -298,8 +328,8 @@ public final class NativeImageUtil {
         }
 
         ByteBuffer buffer = ByteBuffer.allocate(channels);
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 buffer.putInt(array[x][y]);
                 int offset = (x + y * image.width) * channels;
 
